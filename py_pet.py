135e2ffec8f049d3366e8531d634aa30b62c4709
Sonnenbrand <zombie@familiereiter.de>
2020-05-09 10:24:22 +0200

    Erster Code

---
+++ py_pet.py
@@ -0,0 +1,15 @@
+# Small programm that is just interactiong with minimum of functions
+
+name = "knuddeliger kleiner Plüschball"
+bild = "..::oöo::.."
+gewicht = 1
+alter = 1
+hunger = True
+
+# Funktion um den Status des py_pets zu sehen
+# todo Wie ruft der User die Stats auf?
+def stats():
+    print("Ich heiße " + name)
+
+
+stats()
