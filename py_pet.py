f4367823bbf3be3d75858fc3edd855e36ee47d8a
Sonnenbrand <zombie@familiereiter.de>
2020-05-09 12:12:43 +0200

    py_pet jetzt als Dictionary. Follow up todo andere py_pets machen.

--- py_pet.py
+++ py_pet.py
@@ -1,18 +1,19 @@
 # coding=UTF-8
 # Small programm that is just interactiong with minimum of functions with help of
 # https://www.youtube.com/watch?v=TburgnvAPLE
-# todo das olle .idea aus Github entfernen
 
 import random
 
-name = "knuddeliger kleiner Plüschball"
-bild = "..::oöo::.."
-gewicht = 1
-alter = 1
-hunger = True
-beenden = False
-spreuche = ("SchnurrSchnurrSchnurr", "PrrrrrBrrrrPrrrr", "Ich will ein Tamagotchi sein")
+py_pluesch = {
+    "name": "knuddeliger kleiner Plüschball",
+    "bild": "..::oOo::..",
+    "gewicht": 1,
+    "alter": 1,
+    "hunger": True,
+    "sprueche": ["SchnurrSchnurrSchnurr", "PrrrrrBrrrrPrrrr", "Ich will ein Tamagotchi sein"],
+}
 
+beenden = False
 
 # Funktion um den Status des py_pets zu sehen
 
@@ -22,9 +23,9 @@ def start_pet():
 
 
 def stats():
-    print("Ich heiße " + name + " " + bild)
-    print("Ich bin " + str(alter) + " Jahre alt und wiege " + str(gewicht))
-    if hunger:
+    print("Ich heiße " + py_pluesch["name"] + " und so sehe ich aus   " + py_pluesch["bild"])
+    print("Ich bin " + str(py_pluesch["alter"]) + " Jahre alt und wiege " + str(py_pluesch["gewicht"]))
+    if py_pluesch["hunger"]:
         print("UND ICH BIN HUNGRIG!")
     else:
         print("Und ich bin schön satt!")
@@ -32,6 +33,7 @@ def stats():
 
 start_pet()
 
+# todo: Wenn Gewicht größer 10 Pet stirbt aber "Trainieren" macht Gewicht Weniger
 
 while not beenden:
     print("###################################")
@@ -41,15 +43,17 @@ while not beenden:
     elif user_input == "Status":
         stats()
     elif user_input == "Füttern":
-        gewicht = gewicht + 1
-        hunger = False
+        py_pluesch["gewicht"] = py_pluesch["gewicht"] + 1
+        py_pluesch["hunger"] = False
         print("NomNomNom! Das war lecker!")
-        print("Ich wiege jetzt " + str(gewicht))
+        print("Ich wiege jetzt " + str(py_pluesch["gewicht"]))
     elif user_input == "Hallo":
-        print(random.choice(spreuche))
+        print(random.choice(py_pluesch["sprueche"]))
+    elif user_input == "Schau":
+        print(str(py_pluesch["bild"]))
     else:
         # Hier immer alle möglichen User Inputs ergänzen
-        print("Wie bitte? Du kannst nur Status, Füttern, Hallo oder Ende nutzen!")
+        print("Wie bitte? Du kannst nur Status, Füttern, Hallo, Schau oder Ende nutzen!")
 
 
 print("Bis zum nächsten Mal!")
