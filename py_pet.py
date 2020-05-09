9a50d2eae579f3b8357ede6b70cd0ae8ead7b01e
Sonnenbrand <zombie@familiereiter.de>
2020-05-09 10:51:56 +0200

    User Eingabe und Ende hinzugefügt

--- py_pet.py
+++ py_pet.py
@@ -8,8 +8,35 @@ hunger = True
 
 # Funktion um den Status des py_pets zu sehen
 # todo Wie ruft der User die Stats auf?
+
+
+def start_pet():
+    print('Hallo, ich bin dein py_pet!')
+
+
 def stats():
-    print("Ich heiße " + name)
+    print("Ich heiße " + name + " " + bild)
+    print("Ich bin " + str(alter) + " Jahre alt und wiege " + str(gewicht))
+    if hunger:
+        print("UND ICH BIN HUNGRIG!")
+    else:
+        print("Und ich bin schön satt!")
 
 
+def fuettern():
+    print("nix")
+
+
+start_pet()
 stats()
+
+beenden = False
+
+while not beenden:
+    print("###################################")
+    user_input = input('>')
+    if user_input == "Ende":
+        beenden = True
+
+
+print("Bis zum nächsten Mal!")
