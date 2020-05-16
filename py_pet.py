60bdf3d93c7b623e27f45817d2062d185805174b
Sonnenbrand <zombie@familiereiter.de>
2020-05-16 12:53:49 +0200

    Wetter API (achtung API key ist nur lokal und auslesen aus JSON

--- py_pet.py
+++ py_pet.py
@@ -3,6 +3,12 @@
 # https://www.youtube.com/watch?v=TburgnvAPLE
 # todo andere py_pets zur Auswahl machen
 import random
+from settings_api import wetterapikey
+import json
+import requests
+import datetime
+
+
 
 py_pluesch = {
     "name": "knuddeliger kleiner Plüschball",
@@ -12,7 +18,7 @@ py_pluesch = {
     "hunger": True,
     "sprueche": ["SchnurrSchnurrSchnurr", "PrrrrrBrrrrPrrrr", "Ich will ein Tamagotchi sein"],
 }
-
+stadt = "München"
 beenden = False
 
 # Funktion um den Status des py_pets zu sehen
@@ -36,10 +42,20 @@ def tod():
     beenden = True
 
 
+def wetter():
+    response = requests.get("http://api.openweathermap.org/data/2.5/weather?q=" + str(stadt) + "&units=metric&APPID=" + wetterapikey)
+    wetterdaten = response.json()
+    # debugging - wirft JSON daten aus: print(json.dumps(wetterdaten, indent=4, sort_keys=True))
+    print("Ich sage dir das Wetter für die Stadt " + str(stadt) + ".")
+    print("Die Temperatur ist: {}° C".format(str(wetterdaten["main"]["temp"])))
+
+
 start_pet()
+
 
 # todo: Wenn Gewicht größer 10 Pet stirbt aber "Trainieren" macht Gewicht Weniger
 # todo: zu viel training füht zum Tod
+
 
 while not beenden:
     print("###################################")
@@ -60,6 +76,8 @@ while not beenden:
         print(random.choice(py_pluesch["sprueche"]))
     elif user_input == "Schau":
         print(str(py_pluesch["bild"]))
+    elif user_input == "Wetter":
+        wetter()
     elif user_input == "Training":
             if py_pluesch["gewicht"] < 1:
                 py_pluesch["gewicht"] = py_pluesch["gewicht"] - 1
@@ -69,7 +87,7 @@ while not beenden:
                 tod()
     else:
         # Hier immer alle möglichen User Inputs ergänzen
-        print("Wie bitte? Du kannst nur Status, Füttern, Hallo, Training, Schau oder Ende nutzen!")
+        print("Wie bitte? Du kannst nur Status, Füttern, Hallo, Training, Schau, Wetter oder Ende nutzen!")
 
 
 print("Bis zum nächsten Mal!")
